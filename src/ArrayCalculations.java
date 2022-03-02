8dc52be1c15f522da7079e4cd00724450d3fd4e5
AveryCohen <AveryCohen04@gmail.com>
2022-03-02 15:14:19 -0500

    worked on  twodarraycal

--- src/ArrayCalculations.java
+++ src/ArrayCalculations.java
@@ -1,6 +1,15 @@
 public class ArrayCalculations {
 
     public static int rowSum(int[][] arr, int row) {
+
+        if (row > arr.length || row < 0) {
+            return -1;
+        }
+        int total = 0;
+        for (int col = 0; col < arr[row].length; col++) {
+            total += arr[row][col];
+        }
+
         // create a variable to keep track of the total
 
 
@@ -16,10 +25,18 @@ public class ArrayCalculations {
 
 
         // return the total
-        return 0;
+        return total;
     }
 
     public static int columnSum(int[][] arr, int col) {
+        int total = 0;
+        if (col > arr[0].length || col < 0) {
+            return -1;
+        }
+        ;
+        for (int row = 0; row < arr.length; row++) {
+            total += arr[row][col];
+        }
         // create a variable to keep track of the total
 
 
@@ -34,15 +51,33 @@ public class ArrayCalculations {
          */
 
         // return the total
-        return 0;
+        return total;
     }
 
     public static int diagonalSum(int[][] arr, int direction) {
+        if (direction > 1 || direction < 0) {
+            return -1;
+        }
+        if (direction == 1) {
+            int total = 0;
+            int col2 = arr.length;
+            for (int row = 0; row < arr.length; row++) {
+                total += arr[row][col2];
+                col2--;
+            }
 
-        return 0;
+            return total;
+        } else {
+            int total = 0;
+            int col2 = arr.length;
+            for (int row = 0; row < arr.length; row++) {
+                total += arr[row][col2];
+                col2--;
+            }
+
+            return total;
+        }
+
 
     }
-
-
-
 }
